522eb9847902e6eb86c0b25c1fe1d051f9b7d387
Hardec <aleks7777k2@gmail.com>
2021-09-26 15:35:37 +0300

    add base struct of project

---
+++ src/main/java/com/alfa/exchangerate/ExchangerateApplication.java
@@ -0,0 +1,15 @@
+package com.alfa.exchangerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ExchangerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangerateApplication.class, args);
+	}
+
+}
